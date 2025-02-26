f308887abe8a3e41cbbbd9fc0d40107bb0f98e96
Mahitha JV <mahithajv495@gmail.com>
2025-02-26 12:18:50 +0530

    Add files via upload

--- dsa lab/bulidmaxheap.c
+++ dsa lab/bulidmaxheap.c
@@ -1,5 +1,39 @@
 # include<stdio.h>
-void heapify(int a[],int n,int i){
-    int largest =i;
-    
+#include<stdlib.h>
+# include<time.h>
+
+void maxheapify(int a[],int n,int i){
+    int largest=i;
+    int l=2*i+1;
+    int r=2*i+2;
+    while(l<n &&a[l]>a[largest])
+    largest=l;
+    while(r<n && a[r]>a[largest])
+    largest=r;
+if(i!=largest){
+    int tem=a[i];
+    a[i]=a[largest];
+    a[largest]=tem;
+    maxheapify(a,n,largest);
+}
+}
+void bulidmaxheap(int a[],int n){
+    for(int i=n/2-1;i>=0;i--)
+    maxheapify(a,n,i);
+}
+int main(){
+    int n;
+    printf("enter no of ele");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+    a[i]=rand();
+    }
+    double timetaken;
+    clock_t start=clock();
+    bulidmaxheap(a,n-1);
+    clock_t end=clock();
+    timetaken= (double) (end-start)/CLOCKS_PER_SEC;
+    printf("time taken is %f\n",timetaken);
+
 }
